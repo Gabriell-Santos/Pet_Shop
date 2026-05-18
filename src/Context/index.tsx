dc5eb2e0b052be3e92b87df47cc1fc1acec4eabb
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-05-18 20:09:54 -0300

    criada o contextApi e já lingado as rotas

---
+++ src/Context/index.tsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+interface CartItem {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  subTotal: number;
+  cover: string;
+}
+
+interface ContextProps {
+  cart: CartItem[];
+  numberCart: number;
+}
+
+interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+export const userContext = createContext({} as ContextProps);
+
+export function Context({ children }: ChildrenProps) {
+  const [cart, setcart] = useState<CartItem[]>([]);
+  return (
+    <userContext.Provider value={{ cart, numberCart: cart.length }}>
+      {children}
+    </userContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./App";
 import { RouterProvider } from "react-router-dom";
+import { Context } from "./Context/index";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Context>
+      <RouterProvider router={router} />
+    </Context>
   </StrictMode>,
 );
